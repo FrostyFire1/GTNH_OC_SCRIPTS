bc7908f06855f2937f45216698c8d19947c05b4f
FrostyFire1 <66720515+FrostyFire1@users.noreply.github.com>
2024-05-08 14:47:20 +0200

    Scan computer network for LSCs

--- LSC Monitor/main.lua
+++ LSC Monitor/main.lua
@@ -4,8 +4,16 @@ local height = 345
 local width = 640
 
 glasses.removeAll()
-
 print(glasses.getBindPlayers())
+
+local LSC_List = {}
+for address,_ in component.list("gt_machine") do
+    local proxy = component.proxy(address)
+    if address.getName() == "multimachine.supercapacitor" then
+        table.insert(LSC_LIST, proxy)
+    end
+end
+print(LSC_List)
 
 local testWidget = glasses.addTextLabel()
 testWidget.setText("This program is working properly")
