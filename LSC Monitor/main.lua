4144ad0c55e4f133ad2dd719f8d9eff60c33d6f7
FrostyFire1 <66720515+FrostyFire1@users.noreply.github.com>
2024-05-08 14:14:13 +0200

    Begin work on LSC monitor

---
+++ LSC Monitor/main.lua
@@ -0,0 +1,7 @@
+local component = require("component")
+local glasses = component.glasses
+local testWidget = glasses.addTextLabel()
+local height = 345
+local width = 640
+testWidget.setText("This program is working properly")
+testWidget.setPosition(0,345)
