08654a11934212d84cc93feb123400377c4e236a
FrostyFire1 <66720515+FrostyFire1@users.noreply.github.com>
2024-05-08 15:57:41 +0200

    add energy bar border, empty energy bar

--- LSC Monitor/main.lua
+++ LSC Monitor/main.lua
@@ -11,24 +11,32 @@ for a,b in pairs(LSC_List) do
     print(a,b)
 end
 
-local energyBarOffsetX = util.width * 0.1
-local energyBarOffsetY = util.height * 0.95
+local energyBarOffsetX = util.width * 0.03
+local energyBarOffsetY = util.height * 0.99
 local energyBarWidth = 150
-local energyBarHeight = 20
+local energyBarHeight = 15
 local triangleRatio = 0.9
 local borderThickness = 5
 
 local energyBarText = glasses.addTextLabel()
 energyBarText.setText("Energy Monitor by FrostyFire1")
 energyBarText.setScale(1.3)
-energyBarText.setColor(247, 67, 7)
-energyBarText.setPosition(enegryBarOffsetX ,energyBarOffsetY - 30)
+energyBarText.setColor(247/255, 67/255, 7/255)
+energyBarText.setPosition(energyBarOffsetX ,energyBarOffsetY - 40)
 
 
 
 local energyBarBorder = glasses.addQuad()
 energyBarBorder.setVertex(1, energyBarOffsetX, energyBarOffsetY)
 energyBarBorder.setVertex(2, energyBarOffsetX + energyBarWidth*triangleRatio, energyBarOffsetY)
-energyBarBorder.setVertex(3, energyBarOffsetX, energyBarOffsetY - energyBarHeight)
-energyBarBorder.setVertex(4, energyBarOffsetX + energyBarWidth, energyBarOffsetY)
-energyBarBorder.setColor(87 , 7, 247)
+energyBarBorder.setVertex(3, energyBarOffsetX + energyBarWidth, energyBarOffsetY - energyBarHeight)
+energyBarBorder.setVertex(4, energyBarOffsetX, energyBarOffsetY - energyBarHeight)
+energyBarBorder.setColor(247/255 , 9/255, 41/255)
+
+local energyBarEmpty = glasses.addQuad()
+energyBarEmpty.setVertex(1, energyBarOffsetX + borderThickness / 2, energyBarOffsetY - borderThickness / 2)
+energyBarEmpty.setVertex(2, energyBarOffsetX + energyBarWidth*triangleRatio + borderThickness / 2, energyBarOffsetY - borderThickness / 2)
+energyBarEmpty.setVertex(3, energyBarOffsetX + energyBarWidth - borderThickness / 2, energyBarOffsetY - energyBarHeight + borderThickness / 2)
+energyBarEmpty.setVertex(4, energyBarOffsetX - borderThickness / 2, energyBarOffsetY - energyBarHeight + borderThickness / 2)
+energyBarEmpty.setColor(0/255 , 0/255, 0/255)
+-- 9, 41, 247
