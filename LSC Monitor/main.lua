93438f1265039a795f7f38ec46a211b4de6fe985
FrostyFire1 <66720515+FrostyFire1@users.noreply.github.com>
2024-05-08 14:35:30 +0200

    List all players and remove all previously existing components

--- LSC Monitor/main.lua
+++ LSC Monitor/main.lua
@@ -4,6 +4,9 @@ local testWidget = glasses.addTextLabel()
 local height = 345
 local width = 640
 
-print(glasses.getBindPlayers())
+for a,b in pairs(glasses.getBindPlayers()) do
+    glasses.removeAll()
+    print(a,b)
+end
 testWidget.setText("This program is working properly")
 testWidget.setPosition(0,345)
