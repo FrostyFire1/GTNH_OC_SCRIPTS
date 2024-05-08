468f48bc4e25d5334e7f93de33588e06922a6e1e
FrostyFire1 <66720515+FrostyFire1@users.noreply.github.com>
2024-05-08 14:18:07 +0200

    Debug player list linked to glasses terminal

--- LSC Monitor/main.lua
+++ LSC Monitor/main.lua
@@ -3,5 +3,7 @@ local glasses = component.glasses
 local testWidget = glasses.addTextLabel()
 local height = 345
 local width = 640
+
+print(glasses.getBindPlayers())
 testWidget.setText("This program is working properly")
 testWidget.setPosition(0,345)
