cea67940e27bfa2ee919a4707cc08af50ec3304b
FrostyFire1 <66720515+FrostyFire1@users.noreply.github.com>
2024-05-08 14:42:43 +0200

    Move the creation of testWidget post initial removal

--- LSC Monitor/main.lua
+++ LSC Monitor/main.lua
@@ -1,11 +1,12 @@
 local component = require("component")
 local glasses = component.glasses
-local testWidget = glasses.addTextLabel()
 local height = 345
 local width = 640
 
 glasses.removeAll()
 
 print(glasses.getBindPlayers())
+
+local testWidget = glasses.addTextLabel()
 testWidget.setText("This program is working properly")
 testWidget.setPosition(0,345)
