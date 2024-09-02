ee578940fb89aef4c70018c28bd4d2585ba80e38
FrostyFire1 <66720515+FrostyFire1@users.noreply.github.com>
2024-09-02 23:39:17 +0200

    Check for storage container and apiary/alveary

--- Breeder-nator/main.lua
+++ Breeder-nator/main.lua
@@ -3,23 +3,37 @@ local component = require("component")
 
 local breederSide = nil
 local storageSide = nil
-local breederContainer = nil
+local breeder = nil
 -- The program assumes only one adapter and one transposer is present in the network
 local transposer = component.transposer
 for i=0,5 do 
     local invSize = transposer.getInventorySize(i)
-    --Assumption that the hooked up storage container has more than 18 slots
-    if (invSize > 18 and storageSide == nil) then
-        storageSide = i
-    --Alvearies have 9 slots, apiaries have 12 (additional 3 for the frames)
-    else if ((invSize == 12 or invSize == 9) and breederSide == nil) then
-        breederSide = i
+    if(invSize ~= nil) then
+        --Assumption that the hooked up storage container has more than 18 slots
+        if (invSize > 18 and storageSide == nil) then
+            print("Storage container found!")
+            storageSide = i
+        --Alvearies have 9 slots, apiaries have 12 (additional 3 for the frames)
+        elseif ((invSize == 12 or invSize == 9) and breederSide == nil) then
+            breederSide = i
+        end
     end
+end
 
-    if (component.tile_for_alveary and breederContainer == nil) then
-        breederContainer = component.tile_for_alveary
-    else if (component.tile_for_apiculture_0_name and breederContainer == nil) then
-        breederContainer = component.tile_for_apiculture_0_name
-    end
+
+if (next(component.list("for_alveary_0")) ~= nil and breeder == nil) then
+    print("Alveary found!")
+    breeder = component.for_alveary_0
+elseif (next(component.list("tile_for_apiculture_0_name")) ~= nil and breeder == nil) then
+    print("Apiary found!")
+    breeder = component.tile_for_apiculture_0_name
+end
+
+if (breederSide == nil or breeder == nil) then
+    print("Could not find apiary/alveary! Closing.")
+    os.exit()
+elseif (storageSide == nil) then
+    print("Could not find storage container! Closing.")
+    os.exit()
 end
 
